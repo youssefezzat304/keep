eb2cac5f0b1b347311e389b0d3c403fe10901c34
Youssef Abdelrahim <youssefezzatw@gmail.com>
2026-06-20 19:00:49 +0200

    Initial Commit

---
+++ keep/keepApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepApp.swift
+//  keep
+//
+//  Created by Youssef Abdelrahim on 20.06.26.
+//
+
+import SwiftUI
+
+@main
+struct keepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
